dc365d0e4e46d66b77a829207c00a8ca5c70f4cb
MohamedKadi <gamesmohamed14@gmail.com>
2025-01-29 19:45:57 +0100

    userController + routes getAll getOne deleteOne

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,4 +1,53 @@
-exports.all = (req, res, next) => {
+const { isValidObjectId } = require('mongoose');
+const User = require('../models/User');
+
+exports.getAll = async (req, res, next) => {
   try {
-  } catch (error) {}
+    const users = await User.find({});
+    res.status(200).json({
+      length: users.length,
+      data: users,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getOne = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id).select(
+      '-_id name email isVerified dateCreated'
+    );
+    if (!user) {
+      const noUserError = new Error('No user found');
+      noUserError.status = 'No user with this id';
+      noUserError.statusCode = 404;
+      return next(noUserError);
+    }
+    res.status(200).json({
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteOne = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!isValidObjectId(id)) {
+      const idError = new Error('this id does not exists');
+      idError.status = 'fail to delete';
+      idError.statusCode = 404;
+      return next(idError);
+    }
+    const deleted = await User.findByIdAndDelete(id);
+    res.status(200).json({
+      message: 'the user got deleted successfully',
+      user: deleted,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const protectMiddleware = require('../middleware/protect.middleware');
 const userController = require('../Controllers/userController');
 
-router.route('/').get(userController.all);
+router.route('/').get(userController.getAll);
+router
+  .route('/:id')
+  .get(userController.getOne)
+  .delete(protectMiddleware.protectRoute, userController.deleteOne);
 
 module.exports = router;
