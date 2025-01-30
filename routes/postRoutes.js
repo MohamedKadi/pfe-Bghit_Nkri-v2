6cc947b9b76e5d05fc94580ec8530790e08afb60
MohamedKadi <gamesmohamed14@gmail.com>
2025-01-30 20:15:17 +0100

    routes for postRoutes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../Controllers/postController');
 
-router.route('/').get(postController.getAll);
-router.route('/:id').get(postController.getOne);
+router.route('/').get(postController.getAll).post(postController.createOne);
+router.route('/:id').get(postController.getOne).patch(postController.updateOne);
 router.route('/user/:id').get(postController.getAllUser);
 
 module.exports = router;
